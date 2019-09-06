bbbcea6feaa4ec3c73471d2b8066c3c9724affa6
Matthew Weitzel <matthew@Matthews-MacBook-Pro.local>
2019-09-06 07:42:58 -0600

    fix bind, add bad dig

--- foo/lib.go
+++ foo/lib.go
@@ -2,6 +2,7 @@ package foo
 
 import (
 	"reflect"
+	"strings"
 	"sync"
 )
 
@@ -15,20 +16,31 @@ type impl struct {
 	async   bool
 	lastOut interface{}
 	wg      sync.WaitGroup
+	bArgs   []interface{}
 }
 
-func (i *impl) handleCase(arg cheat) interface{} {
+func (i *impl) handleCase(arg cheat) *impl {
 	if arg["async"] == true {
 		i.async = true
 	} else {
 		i.async = false
 	}
 
+	bArgs, ok := arg["bind"]
+	if ok {
+		ix := *i
+		i = &ix
+		i.bArgs = append([]interface{}{}, i.bArgs...)
+		i.bArgs = append(i.bArgs, bArgs.([]interface{})...)
+		return i
+	}
+
 	if arg["await"] == true {
 		i.wg.Wait()
-		return i.lastOut
+		//		return i.lastOut
+		return i
 	}
-	return nil
+	return i
 }
 
 func (i *impl) special() func(args ...interface{}) interface{} {
@@ -40,12 +52,15 @@ func (i *impl) special() func(args ...interface{}) interface{} {
 			}
 		}
 
+		xInputs := []interface{}{}
+		xInputs = append(xInputs, i.bArgs...)
+		xInputs = append(xInputs, args...)
 		if i.async {
 			i.wg.Add(1)
 			go func() {
 				defer i.wg.Done()
 				inputs := []reflect.Value{}
-				for _, arg := range args {
+				for _, arg := range xInputs {
 					inputs = append(inputs, reflect.ValueOf(arg))
 				}
 				outs := reflect.ValueOf(i.fn).Call(inputs)
@@ -57,7 +72,7 @@ func (i *impl) special() func(args ...interface{}) interface{} {
 			return nil
 		} else {
 			inputs := []reflect.Value{}
-			for _, arg := range args {
+			for _, arg := range xInputs {
 				inputs = append(inputs, reflect.ValueOf(arg))
 			}
 			outs := reflect.ValueOf(i.fn).Call(inputs)
@@ -72,7 +87,8 @@ func (i *impl) special() func(args ...interface{}) interface{} {
 type mySpecial func(...interface{}) interface{}
 
 func (ms mySpecial) Await(args ...interface{}) interface{} {
-	return ms(cheat{"await": true})
+	i := ms(cheat{"await": true}).(*impl)
+	return i.lastOut
 }
 
 func (ms mySpecial) Async() mySpecial {
@@ -82,8 +98,66 @@ func (ms mySpecial) Async() mySpecial {
 	}
 }
 
+func (ms mySpecial) Bind(bArgs ...interface{}) mySpecial {
+	// problematic, this does not duplicate impl
+	i := ms(cheat{"bind": bArgs}).(*impl)
+	return i.special()
+}
+
 type cheat map[string]interface{}
 
 type special func(...interface{}) interface{}
 
 //func Async
+
+func Digf(obj interface{}, path string) mySpecial {
+	return F(Dig(obj, path))
+}
+
+func Dig(obj interface{}, path string) interface{} {
+	current := obj
+	for _, v := range strings.Split(path, ".") {
+		rStruct := reflect.ValueOf(current)
+		if rStruct.Kind() == reflect.Struct {
+			field := rStruct.FieldByName(v)
+			if field.Kind() != reflect.Invalid {
+				if !IsZero(field) {
+					current = field.Interface()
+					continue
+				}
+			}
+			method := rStruct.MethodByName(v)
+			if method.Kind() != reflect.Invalid {
+				//			fmt.Println("..............", method)
+				//			fmt.Println("..............", method.Kind())
+				//			fmt.Println("..............", method.IsValid())
+				//			fmt.Println("..............", method.CanInterface())
+				//			fmt.Printf(".............%#v\n", method.Interface())
+				//			fmt.Printf(".............%#v\n", method.Interface())
+				current = method.Interface()
+				continue
+			}
+		}
+		return nil
+	}
+	return current
+}
+
+// go 1.11 bullshit, bump go and use reflect.Value.IsZero()
+func IsZero(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Array, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Interface, reflect.Map, reflect.Ptr, reflect.Slice:
+		return v.IsNil()
+	}
+	return false
+}
--- foo/lib_test.go
+++ foo/lib_test.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"fmt"
 	. "mymod/foo"
 	"time"
 )
@@ -26,9 +27,52 @@ var _ = Describe("Foos", func() {
 			Ω(fBar.Await()).Should(Equal(6))
 		})
 	})
+
+	Describe("Bind/Call/Apply", func() {
+		It("bind 1", func() {
+			Ω(F(bar).Bind(3)()).Should(Equal(6))
+		})
+		It("bind 2", func() {
+			Ω(F(baz).Bind(1, 1, 1)()).Should(Equal(3))
+		})
+
+		It("bind non-statefully", func() {
+			// wrongly statefull, how can I duplicate impl only sometimes?
+			fBaz := F(baz).Bind(1)
+			boundFbaz := fBaz.Bind(1, 1, 1)
+			Ω(fBaz()).Should(Equal(1))
+			Ω(boundFbaz()).Should(Equal(4))
+		})
+	})
+
+	Describe("dig bag", func() {
+		It("can get stuff", func() {
+			type B struct{ Bar int }
+			a := struct{ Foo B }{Foo: B{Bar: 5}}
+			Ω(Dig(a, `Foo.Bar`)).Should(Equal(5))
+			Ω(Dig(a, `Foo.Bat`)).Should(BeNil())
+			var b binterface = bstruct{}
+			Ω(fmt.Sprintf("%#v", Dig(b, `Foo`))).Should(ContainSubstring("(func(int) int)"))
+			Ω(Digf(b, `Foo`)(30)).Should(Equal(60))
+			//		Ω(Dig(a, `Foo.Bar`)).Should(Equal(5))
+		})
+	})
 })
+
+type binterface interface{ Foo(int) int }
+type bstruct struct{}
+
+func (b bstruct) Foo(i int) int { return 2 * i }
 
 func bar(i int) int {
 	time.Sleep(20 * time.Millisecond)
 	return 2 * i
+}
+
+func baz(args ...int) int {
+	carry := 0
+	for _, a := range args {
+		carry += a
+	}
+	return carry
 }
